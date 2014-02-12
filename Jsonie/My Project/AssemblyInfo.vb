d2cbbf14eeac50e3cc51210b3f7c09ecec9914d9
Michal Novak <michal.novak@weborama.cz>
2014-02-12 17:51:33 +0100

    added MIT license

--- Jsonie/My Project/AssemblyInfo.vb
+++ Jsonie/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("Json Data Encoding/Decoding Framework")> 
 <Assembly: AssemblyCompany("Dextronet")> 
 <Assembly: AssemblyProduct("Jsonie")> 
-<Assembly: AssemblyCopyright("")> 
+<Assembly: AssemblyCopyright("Copyright (c) 2014 Michal Novák, Dextronet")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
